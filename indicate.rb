eb99c982dcee3789f46a8995144af6a01b8e1542
rosedive <sakindiuweramarie@gmail.com>
2019-08-16 15:34:02 -0700

    addition of HTML page

--- indicate.rb
+++ indicate.rb
@@ -1,18 +1,13 @@
-# Launch CGI program to receive and return data
 require 'cgi'
 cgi = CGI.new
-
-# After receiving data, return a response in HTML format
+​
 cgi.out("type" => "text/html", "charset" => "UTF-8") {
-  # Fetch received data cgi ['input'] and assign to local variable
-  # Retrieve information from the marker 'input'
-  get = cgi['input']
-
-  # Return the response in HTML
-  "<html>
-    <body>
-      <p>The received string is as follows</p>
-      <p>String：#{get}</p>
-    </body>
-  </html>"
-}
+   
+    get = cgi['input']
+    "<html>
+      <body>
+        <p>The received string is as follows</p>
+        <p>String：#{get}</p>
+      </body>
+    </html>"
+  }
